c2fa2897d08f81371791897c617781ceab0be57e
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-02 11:47:39 +0300

    Squares can be drawn on the drawing board

---
+++ script.js
@@ -0,0 +1,31 @@
+let drawingBoard = document.querySelector('.drawing-board');
+
+// Create a square div board
+
+/* 
+ * By default create a 16 x 16 div
+ * 
+ * How? Iterate through numSquare and create a div and append to each other. 
+ *  
+ * 
+ */
+
+function drawSquare (numSquare = 32) { //numSquare is the size of the square matrix.(numSquare=16 means it will create a 16x16 square)
+    
+    for (let i = 1; i <= numSquare; i++) {
+        let square = document.createElement(`div`);
+        square.classList.add(`square`);
+        square.classList.add(i);
+        for (let i = 1; i <= numSquare; i++) {
+            let squareSquare = document.createElement(`div`);
+            squareSquare.classList.add(`square-square`);
+            squareSquare.classList.add(i);
+            square.appendChild(squareSquare);
+        }
+        
+        drawingBoard.appendChild(square);
+    }
+
+}
+
+drawSquare();
