7802fa58b48c0b741afdb819840bdc5f60cc2c7f
Betsue Tsegay Weldemariam <32789057+betsha1830@users.noreply.github.com>
2023-01-26 15:08:33 +0300

    Changed event from mousedown to mouseenter

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ updateBtn.addEventListener('click', () => {
 });
 
 squares.forEach(square => {
-    square.addEventListener('mousedown', () => { //drawing for the default size
+    square.addEventListener('mouseenter', () => { //drawing for the default size
         r = Math.round((Math.random()) * 100 + 1);
         g = Math.round((Math.random()) * 100 + 1);
         b = Math.round((Math.random()) * 100 + 1);
